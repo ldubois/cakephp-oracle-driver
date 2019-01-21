0ebec3b14e7aac55d772c864152879896c60846d
master <ludoivc.dubois89@gmail.com>
2019-01-21 09:11:38 +0100

    Optimisation requete sur les indexes

--- src/Database/Schema/OracleSchema.php
+++ src/Database/Schema/OracleSchema.php
@@ -451,16 +451,11 @@ WHERE 1=1 " . ($useOwner ? $ownerCondition : '') . $objectCondition . " ORDER BY
 
         $sql = "SELECT
             ic.index_name AS name,
-            (
-                SELECT i.index_type
-                FROM   $indexesTable i
-                WHERE  i.index_name = ic.index_name" . ($useOwner ? ' AND ic.table_owner = i.table_owner' : '') . "
-            ) AS type,
+            i.index_type AS type,
             decode(
                 (
-                     SELECT i.uniqueness
-                     FROM   $indexesTable i
-                     WHERE  i.index_name = ic.index_name" . ($useOwner ? ' AND ic.table_owner = i.table_owner' : '') . "
+                      i.uniqueness
+                     
                 ),
                 'NONUNIQUE', 0,
                 'UNIQUE', 1
@@ -468,11 +463,12 @@ WHERE 1=1 " . ($useOwner ? $ownerCondition : '') . $objectCondition . " ORDER BY
             ic.column_name AS column_name,
             ic.column_position AS column_pos,
             (
-                SELECT c.constraint_type
-                FROM   $constraintsTable c
-                WHERE  c.constraint_name = ic.index_name" . ($useOwner ? ' AND c.owner = ic.index_owner' : '') . "
+                c.constraint_type
+               
              ) AS is_primary
              FROM $indexColumnsTable ic
+			 join $indexesTable i on  i.index_name = ic.index_name" . ($useOwner ? ' AND ic.table_owner = i.table_owner' : '') . "
+			 join  $constraintsTable c on c.constraint_name = ic.index_name" . ($useOwner ? ' AND c.owner = ic.index_owner' : '') . "
              WHERE ic.table_name = :tableParam" . ($useOwner ? ' AND ic.table_owner = :ownerParam' : '') . "
             ORDER BY ic.column_position ASC";
 
